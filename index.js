5c2c258d417ece76bc828eda85a9ae322fccb4c4
Josie Barth <josie.barth@gmail.com>
2016-11-13 20:55:09 -0500

    updated GET /

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+// mySQL setup
+// TODO root user
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root'
+});
+
+connection.connect();
+
+// Serve static content
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -11,6 +22,19 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get("/test", function(request, response) {
+    console.log("test");
+
+    connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+      if (err) throw err
+
+      console.log('The solution is: ', rows[0].solution);
+    })
+
+    connection.end();
+
 });
 
 app.listen(app.get('port'), function() {
